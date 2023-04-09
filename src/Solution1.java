7544540b83c9ea0b98d0a2c0d5b02cd93ed528f6
luisotvbatista <luisotvbatista@outlook.com>
2023-04-09 17:19:00 -0300

    exercicio Welcome to Java HackerRank

---
+++ src/Solution1.java
@@ -0,0 +1,11 @@
+public class Solution1 {
+
+    //Exercise Welcome to Java! Plataforma HackerRank
+
+        public static void main(String[] args) {
+
+            System.out.println("Hello, World.");
+            System.out.println("Hello, Java.");
+        }
+    }
+
